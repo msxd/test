4f0d82533e6032d1ff7caee1e0a1b50bbd50ac80
msxd <valikov.ids@gmail.com>
2014-04-10 18:32:53 +0300

    Update app.js
    Добавлены:
    комментарии,
    обработка ошибок сервера,
    передача сообщений о статусе между страницами

--- js/app.js
+++ js/app.js
@@ -2,38 +2,56 @@
  * Created by root on 09.04.14.
  */
 
-	//$.cookie('token')
-	//клиентская валидация
-	//
-	//ответы от сервера
-	/*
+	/* клиентская валидация
+	 * Адаптивная верстка
 	 * type for show: danger, warning, success, info
 	 */
+
+//куки токена
 var t_cookie = $.cookie('token');
+
 //узнаем адрес
-var myLocation = function(ur){
+var myLocation = function (ur) {
 	var ar = ur.split('/');
-	return ar[ar.length -1];
-}
+	return ar[ar.length - 1];
+};
+
 //проверка на сущесвование
 var undefCheck = function(check){
-	if(typeof check !== 'undefined')
-		return true;
-	else
-		return false;
+	return typeof check !== 'undefined';
 }
+
+//обработка массива ошибок с сервера
+var showErrors = function (arr) {
+	var makeErrorLog = '';
+	var c = 0;
+	arr.forEach(function (entry) {
+		c += 1;
+		makeErrorLog += c + ") " + entry + "<br/>";
+	});
+	show('danger', makeErrorLog, true);
+};
+
 //если токен существует устанавливаем хедеры в запросы
 if(typeof t_cookie !== 'undefined'){
 	$.ajaxSetup({
 		headers:{Authorization: $.cookie('token')}
 	})
 }
-//вывод общих сообщений
-var show = function(type, message){
-	$('#top').after('<div class="alert alert-' + type + '" id="alert">' + message + '</div>');
-	$('#alert').slideUp(4000);
-}
 
+//вывод сообщений
+var show = function (type, message, c) {
+	$('#top').after('<div class="alert alert-' + type + '" id="alert">' + message + '</div>');
+	if (c) {
+		$('#alert').click(function () {
+			$(this).fadeOut(1000);
+		})
+	} else {
+		$('#alert').slideUp(4000);
+	}
+};
+
+//вывод сообщений с других страниц
 if(undefCheck($.cookie('message_type'))&&undefCheck($.cookie('message_mess'))){
 	show($.cookie('message_type'), $.cookie('message_mess'));
 	$.removeCookie('message_type');
@@ -92,14 +110,16 @@ switch(myLocation(window.location.href)){
 		break;
 }
 
-//лог аут
+//лог аут, вывод сообщения
 $('#logout').click(function(){
 	$.removeCookie('token');
+	$.cookie('message_type',"info");
+	$.cookie('message_mess','You are logout');
 	window.location.href = "../test/index.html"
 });
 
 
-//авторизация
+//авторизация, обработка ошибок сервера, вывод сообщения в случае успеха
 $('#btn_login').click(function(){
 	var flag1 = false;
 	var flag2 = false;
@@ -120,13 +140,16 @@ $('#btn_login').click(function(){
 					$.cookie('message_type','success');
 					$.cookie('message_mess','Welcome!');
 					window.location.href= '../test/main.html';
+				},
+				error: function(data){
+					showErrors(data.responseJSON.errors);
 				}
 			}
 		);
 	}
 });
 
-//регистрация
+//регистрация обработка ошибок сервера, вывод сообщения в случае успеха
 $('#btn_signup').click(function(){
 	var form = $('#login').serialize();
 	$.ajax({
@@ -135,17 +158,18 @@ $('#btn_signup').click(function(){
 		data: form,
 		success: function(data){
 			$.cookie('token',data.token);
+			$.cookie('message_type',"info");
+			$.cookie('message_mess','You are signed up');
 			window.location.href = '../test/main.html'
 		},
-		error: function(xhr, str){
-			console.log(xhr);
-			console.log(str);
+		error: function(data){
+			showErrors(data.responseJSON.errors);
 		}
 	});
 });
 
 
-//test
+//update data обрабатывает ошибки сервера, выводит сообщение в случае успеха
 $('#btn_update').click(function(){
 	var form = $('#form_profile').serialize();
 	$.ajax(
@@ -159,67 +183,10 @@ $('#btn_update').click(function(){
 				$('#inp_last_name').val(data.user['last_name']);
 				$('#inp_phone').val(data.user['phone']);
 				$('#inp_fb').val(data.user['facebook_id']);
+				show('success','Success!',false);
 			}
 		}
-	);
+	).fail(function(data,st1,st2){
+			showErrors(data.responseJSON.errors);
+		});
 });
-/*
-auth
-	$('#c1').click(function(){
-		alert(24);
-		$.ajax(
-			{
-				type: 'POST',
-				url: 'http://192.168.0.122/api/user/signin',
-				headers:{'Authorization':'Basic '+ btoa('client@mail.com:123456')},
-				success: function (data){
-					$.cookie('token',data.token);
-					$('#tok').val('Basic ' + $.cookie('token'));
-				}
-			}
-		);
-
-
-	});
-
-//get datas
-	$('#c2').click(function()
-	{
-		$('#tok').val('Token ' + $.cookie('token'));
-		$.ajax(
-			{
-				type: 'GET',
-				url: 'http://192.168.0.122/api/user/profile',
-				headers:{Authorization: $.cookie('token')}
-			}
-		).done(function(){
-				alert("FAIL");
-			}).fail();
-	});
-
-
-//send data
-	$('#c3').click(function()
-	{
-		$.ajax(
-			{
-				type: 'POST',
-				url: 'http://192.168.0.122/api/user/update',
-				headers:{Authorization: $.cookie('token')},
-				data:{'first_name':'12'},
-				success: function (data){
-					console.log(data);
-				}
-			}
-		).fail(function(){
-				alert("FAIL");
-			});
-	});
-
-$('#c4').click(function(){
-	alert('removed');
-	$(this).html('c4');
-	$.removeCookie('token');
-});
-
-*/
